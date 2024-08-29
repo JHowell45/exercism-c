2189c9d89a213a412e676f97267e5410f3b6ae43
Jacob <jacob@notactivelylooking.com>
2024-08-29 13:16:53 +0100

    Clean formatting.

--- exercises/hello_world/tests/hello_world.c
+++ exercises/hello_world/tests/hello_world.c
@@ -17,7 +17,6 @@ void tearDown(void)
 // Defines a single test.
 static void test_say_hi(void)
 {
-
    // Check if the 'hello()' function returns "Hello, World!"
    // This test is expected to fail after first downloading this exercise.
    // To make this test pass, fix the 'hello()' function definition in the
