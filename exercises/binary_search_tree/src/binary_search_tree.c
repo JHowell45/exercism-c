574f4b0151cde9085943dd56e7091862ad0ffda3
Jacob <jacob@notactivelylooking.com>
2024-08-29 20:14:44 +0100

    Fixing include and basic methods.

--- exercises/binary_search_tree/src/binary_search_tree.c
+++ exercises/binary_search_tree/src/binary_search_tree.c
@@ -1,3 +1,11 @@
-#include <stdio.h>
-#include <math.h>
-#include "grains.h"
+#include "binary_search_tree.h"
+
+node_t *build_tree(int *tree_data, size_t tree_data_len)
+{
+}
+void free_tree(node_t *tree)
+{
+}
+int *sorted_data(node_t *tree)
+{
+}
