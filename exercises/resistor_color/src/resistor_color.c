ec1e3ec57a865d3375f6939c9827b35fdac645d7
Jacob <jacob@notactivelylooking.com>
2024-08-30 13:17:58 +0100

    Creating base methods for the enum.

--- exercises/resistor_color/src/resistor_color.c
+++ exercises/resistor_color/src/resistor_color.c
@@ -1,1 +1,14 @@
+#include <stdlib.h>
+
 #include "resistor_color.h"
+
+uint16_t color_code(resistor_band_t color)
+{
+   return color;
+}
+
+const resistor_band_t *colors(void)
+{
+   resistor_band_t *c = calloc(10, sizeof(resistor_band_t));
+   return c;
+}
