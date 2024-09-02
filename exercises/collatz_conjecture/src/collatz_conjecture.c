d57755d720ae7a2dea9916346e87741ddc362273
Jacob <jacob@notactivelylooking.com>
2024-09-02 14:03:11 +0100

    Handled exceptions.

--- exercises/collatz_conjecture/src/collatz_conjecture.c
+++ exercises/collatz_conjecture/src/collatz_conjecture.c
@@ -4,14 +4,13 @@
 
 int steps(int start)
 {
+   if (start <= 0) {
+      return ERROR_VALUE;
+   }
    int steps = 0;
    int n = start;
    while (n != 1) {
-      if (n % 2 == 0) {
-         n /= 2;
-      } else {
-         n = (n * 3) + 1;
-      }
+      n = n % 2 == 0 ? n / 2 : n * 3 + 1;
       steps++;
    }
    return steps;
