2b93f34599b25f168efd32d25422612d0515b930
Jacob <jacob@notactivelylooking.com>
2024-09-02 13:50:21 +0100

    basic impl.

--- exercises/collatz_conjecture/src/collatz_conjecture.c
+++ exercises/collatz_conjecture/src/collatz_conjecture.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+
 #include "collatz_conjecture.h"
 
 int steps(int start)
 {
+   int steps = 0;
+   int n = start;
+   while (n != 1) {
+      if (n % 2 == 0) {
+         n /= 2;
+      } else {
+         n = (n * 3) + 1;
+      }
+      steps++;
+   }
+   return steps;
 }
